6b01b74f8718ff2165acaf5b1f098cd81e6b30b1
Dino-Sven Dedić <dino@procerasoft.com>
2022-03-27 13:45:38 +0200

    deploy preparation fix

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,8 +2,10 @@ import axios from "axios";
 
 class Request {
     static DefaultSettings = {
-        Protocol: "http",
-        BaseUrl: process.env.REACT_APP_BACKEND_URL,
+        Protocol: process.env.REACT_APP_PROTOCOL ?? "https",
+        BaseUrl:
+            process.env.REACT_APP_BACKEND_URL ??
+            "ask-it-task-app.herokuapp.com/api",
         Headers: {
             "Content-Type": "application/json",
             "x-auth-token": JSON.parse(localStorage.getItem("user"))?.token,
